9154f3912c5166344a9a12da37efc343215aa5ed
Leedonguk <donguk0105@naver.com>
2023-07-04 19:21:00 +0900

    자바의 정석 Chapter2

---
+++ src/main/java/org/example/chapter2/ex8.java
@@ -0,0 +1,16 @@
+package org.example.chapter2;
+
+public class ex8 {
+    public static void main(String[] args) {
+
+        String name = "ja" + "va";
+        /**
+         * 문자열 String ""(쌍 따옴표) 사용하여 초기화
+         * 문자열은 합치기 가능
+         * char 와 달리
+         * String empty = "";
+         * 를 허용함 즉, 빈 문자열이 가능하다.
+         * char 문자는 반드시 하나의 문자가 있어야 함. 공백 가능
+         */
+    }
+}
