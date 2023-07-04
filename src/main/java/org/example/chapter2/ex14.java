b2e3982f112c4cf7a0d1094a54956c8ebe32916e
Leedonguk <donguk0105@naver.com>
2023-07-04 19:21:32 +0900

    자바의 정석 Chapter2

---
+++ src/main/java/org/example/chapter2/ex14.java
@@ -0,0 +1,19 @@
+package org.example.chapter2;
+
+import java.util.Scanner;
+
+public class ex14 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String st = sc.next();
+
+        int num = Integer.parseInt(st);
+//        Integer.parseInt() 문자열을 숫자로 변환하는 메서드
+//        Double.parseDouble() 역시 문자열을 숫자로 변환하는 메서드
+
+
+
+    }
+}
---
+++ src/main/java/org/example/chapter2/ex9.java
@@ -0,0 +1,20 @@
+package org.example.chapter2;
+
+public class ex9 {
+    public static void main(String[] args) {
+//        두 변수의 값 바꾸기
+
+        int x = 10, y = 5;
+        int z; // 임시로 값을 저장할 공간
+
+        z = x;  // z == 10
+        x = y;  // x == 5
+        y = z;  // y == 10
+
+
+
+
+
+
+    }
+}
