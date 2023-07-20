e6be922b08028cef77b77c19ba1c21a4b1142b6e
Leedonguk <donguk0105@naver.com>
2023-07-20 15:05:55 +0900

    자바의 정석 7장 extend

---
+++ src/main/java/org/example/chapter7/tv/Tv.java
@@ -0,0 +1,18 @@
+package org.example.chapter7.tv;
+
+public class Tv {
+    boolean power;
+    int channel;
+
+    void power() {
+        power = !power;
+    }
+
+    void channelUp() {
+        ++channel;
+    }
+
+    void channelDown() {
+        --channel;
+    }
+}
